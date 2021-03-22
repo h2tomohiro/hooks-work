912453649bd0d2ffa5961f6a74de89ac9b2139ee
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-03-22 10:07:27 -0700

    change app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react'
 import ToDoList from './components/ToDoList'
 import './styles.css'
 import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-function App({ toDoList }) {
+function App() {
+  const toDoList = useSelector((state) => state.toDoList)
   return (
     <div className='container'>
       <h1 className='center blue-text'>ToDo's ({toDoList.length})</h1>
@@ -11,11 +13,4 @@ function App({ toDoList }) {
     </div>
   )
 }
-
-const mapStateToProps = (state) => {
-  return {
-    toDoList: state.toDoList,
-  }
-}
-
-export default connect(mapStateToProps)(App)
+export default App
